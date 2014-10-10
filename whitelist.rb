c5c5028bf3abd5144e23cff76c741ae7c598901b
duccioarmenise <duccioarmenise@gmail.com>
2014-10-10 17:24:42 +0200

    txts moved in txt folder

--- whitelist.rb
+++ whitelist.rb
@@ -2,7 +2,7 @@ class Whitelist
 
   def initialize
     @words = []
-    File.readlines('privacy_whitelist_it.txt').each do |word|
+    File.readlines('txt/privacy_whitelist_it.txt').each do |word|
       word.chomp!
       @words << word unless word.nil? || word.empty?
     end
