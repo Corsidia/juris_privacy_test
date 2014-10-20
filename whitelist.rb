6f86060f3ebe0d691dda7f35c5dc8a6c9361e559
duccioarmenise <duccioarmenise@gmail.com>
2014-10-20 19:06:03 +0200

    stable

--- whitelist.rb
+++ whitelist.rb
@@ -2,12 +2,24 @@ class Whitelist
 
   def initialize
     @words = []
-    File.readlines('txt/privacy_whitelist_it.txt').each do |word|
+    File.readlines('txt/whitelist_words_it.txt').each do |word|
+      word.chomp!
+      @words << word unless word.nil? || word.empty?
+    end
+
+    @prefixes = []
+    File.readlines('txt/whitelist_suffixes_it.txt').each do |word|
+      word.chomp!
+      @words << word unless word.nil? || word.empty?
+    end
+
+    @suffixes = []
+    File.readlines('txt/whitelist_prefixes_it.txt').each do |word|
       word.chomp!
       @words << word unless word.nil? || word.empty?
     end
   end
 
-  attr_reader :words
+  attr_reader :words, :suffixes, :prefixes
 
 end
